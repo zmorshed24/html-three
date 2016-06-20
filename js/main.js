48d0eb3fa4f38701ceb5cdc9d54176ab558ebf0e
zmorshed24 <zulfiker_morshed@yahoo.com>
2016-06-21 00:21:23 +0600

    again change
    again change

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('ul#nav_two').slicknav();
+	
+	$('#slides').superslides({
+		play: 2000,
+		pagination: false
+	})
+	
+		$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+});
